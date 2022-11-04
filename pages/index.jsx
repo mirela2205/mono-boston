9aa6bac71e8e9d489c54256e9ec3b8ac9ca4f26f
mirela2205 <mirela.dina88@gmail.com>
2022-11-04 17:46:22 +0200

    test

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      hello monochrome
+      <i className="fa-regular fa-face-smile"></i>
+      <img src="/images/banners/banner.png" alt="banner"></img>
+    </div>
+  );
+}
