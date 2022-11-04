86a94edaf7f7e93857406b78a14fbb199dc2c8ac
mirela2205 <mirela.dina88@gmail.com>
2022-11-04 21:47:47 +0200

    mono test

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,15 @@ export default function Home() {
       hello monochrome
       <i className="fa-regular fa-face-smile"></i>
       <img src="/images/banners/banner.png" alt="banner"></img>
+      <button className="button button-primary button-large">
+        <a href="/">View all</a>
+      </button>
+      <button className="button button-primary button-inverted">
+        <a href="/">Submit</a>
+      </button>
+      <button className="button button-secondary">
+        <a href="/">New in</a>
+      </button>
     </div>
   );
 }
